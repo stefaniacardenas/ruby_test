d84792fd662d3828e07b0f795869bc2df38c6271
stefaniacardenas <stefaniaferullo@hotmail.com>
2014-06-04 21:41:11 +0100

    'get_upper_limit_of' passes

--- questions_spec.rb
+++ questions_spec.rb
@@ -174,12 +174,12 @@ describe 'the Friday test :)' do
     expect(b).to be_true
   end
 
-  specify 'get_upper_limit_of' do
+  fit 'get_upper_limit_of' do
     n = get_upper_limit_of 1..20
     expect(n).to eq 20
   end
 
-  fit 'is_a_3_dot_range?' do
+  it 'is_a_3_dot_range?' do
     a = is_a_3_dot_range? 1..20
     b = is_a_3_dot_range? 1...20
 
